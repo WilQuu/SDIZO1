5cb567f07882d3d5b37d16bf73e1f7bfb21e9382
WilQuu <50849704+WilQuu@users.noreply.github.com>
2020-04-12 13:16:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "Array.cpp"
+#include "List.h"
+#include "Heap.h"
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    Array array;
-    for (int i = 0; i < 10; i++) {
-        array.addEnding(i);
+    Heap heap(20);
+    for (int i = 0; i < 15; i++) {
+        //int randomInt = rand() % 30 + 1;
+        heap.push(i);
     }
-    array.printArray();
+    heap.printHeap();
+    cout << endl;
+    heap.pop();
+    heap.printHeap();
     return 0;
 }
