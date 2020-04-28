994ba043ef9348f49855e3e4068883d94e1540bd
WilQuu <50849704+WilQuu@users.noreply.github.com>
2020-04-28 15:07:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
-#include <iostream>
-#include "Array.cpp"
-#include "List.h"
-#include "Heap.h"
-#include <stdlib.h>
-#include <time.h>
+#include "Menu.h"
+
+using namespace std;
 
 int main() {
-    Heap heap(20);
-    for (int i = 0; i < 15; i++) {
-        //int randomInt = rand() % 30 + 1;
-        heap.push(i);
-    }
-    heap.printHeap();
-    cout << endl;
-    heap.pop();
-    heap.printHeap();
+    Menu menu;
+    menu.printMenu();
     return 0;
 }
